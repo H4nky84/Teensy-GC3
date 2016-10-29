251cc887f4bfe6823d878fd03465567a6c40e764
H4nky84 <daniel.hancock@gmail.com>
2016-10-29 18:18:58 +1100

    Update mode_cmds.ino

--- mode_cmds.ino
+++ mode_cmds.ino
@@ -17,6 +17,8 @@
 void cmd_rmode(void) {
   mode_word.byte = EEPROM.read(EE_MW);
   imax = ee_read_short(EE_IMAX);
+  inactiveTimeout = ee_read_short(EE_INACTIVE_TIMEOUT);
+  activeTimeout = ee_read_short(EE_ACTIVE_TIMEOUT);
 }
 
 //
@@ -26,7 +28,8 @@ void cmd_rmode(void) {
 //
 void cmd_wmode(void) {
   ee_write_short(EE_IMAX, imax);
+  ee_write_short(EE_INACTIVE_TIMEOUT, inactiveTimeout);
+  ee_write_short(EE_ACTIVE_TIMEOUT, activeTimeout);
   EEPROM.update(EE_MW, mode_word.byte);
   EEPROM.update(EE_MAGIC, MAGIC);
 }
-
